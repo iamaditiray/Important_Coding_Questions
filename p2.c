c1c0a43d2970ada9ff2525ced03c0c1f7ddb23e0
Aditi Ray <imakeothersmile@gmail.com>
2023-09-10 22:38:46 +0530

    Second Commit

---
+++ p2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ unsigned int factorial(int n){
+    int i=n;
+    int fact=1;
+    while(n/i!=n){
+        fact=fact*i;
+        i--;
+    }
+    return fact;
+ }
+
+ int main(){
+    int num;
+    printf("Enter a Number: ");
+    scanf("%d",&num);
+    printf("The factorial of %d id %d", num, factorial(num));
+ }
