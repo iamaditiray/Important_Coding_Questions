cc18dedb183facd0ddb4e7545e1c7b9a2a784536
Aditi Ray <imakeothersmile@gmail.com>
2023-09-10 22:14:37 +0530

    Factorial

--- p1.c
+++ p1.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int factorial(int n);
+unsigned int factorial(int n);
 int main(){
     int a;
     printf("Enter a number: ");
     scanf("%d", &a);
     printf("Factorial of %d is %d", a, factorial(a));
 }
-int factorial(int n){
+unsigned int factorial(int n){
     int fact=1;
     for(int i=2; i<=n; i++){
         fact*=i;
